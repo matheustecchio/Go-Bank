3e921d3c4f1fa6369cbf617b503513812aabce71
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-07-12 16:39:42 +0100

    create new package `fileops` to handle file operations

--- projects/go-bank/fileops/fileops.go
+++ projects/go-bank/fileops/fileops.go
@@ -1,4 +1,4 @@
-package main
+package fileops
 
 import (
 	"fmt"
@@ -6,12 +6,12 @@ import (
 	"strconv"
 )
 
-// createFile checks if a file exists. If it doesn't, it creates the file with the designated name and format,
+// CreateFile checks if a file exists. If it doesn't, it creates the file with the designated name and format,
 // and inserts the `initialValue` into it.
 //
 // `file`: The name of the file to be created.
 // `initialValue`: The initial value to be written into the file.
-func createFile(file string, initialValue string) error {
+func CreateFile(file string, initialValue string) error {
 	if _, err := os.ReadFile(file); err != nil {
 		if _, err := os.Create(file); err != nil {
 			return err
@@ -24,12 +24,12 @@ func createFile(file string, initialValue string) error {
 	return nil
 }
 
-// getFloatFromFile reads a file and returns its content as a float64 value.
+// GetFloatFromFile reads a file and returns its content as a float64 value.
 //
 // `file`: The name of the file to be read.
 //
 // Returns the content of the file as a float64 value and an error if any operation fails.
-func getFloatFromFile(file string) (float64, error) {
+func GetFloatFromFile(file string) (float64, error) {
 	dataByte, err := os.ReadFile(file)
 	if err != nil {
 		return 0, err
@@ -44,11 +44,11 @@ func getFloatFromFile(file string) (float64, error) {
 	return dataFloat, nil
 }
 
-// writeFloatToFile writes a float64 value to a file.
+// WriteFloatToFile writes a float64 value to a file.
 //
 // `file`: The name of the file to be written to.
 // `targetFloat`: The float64 value to be written into the file.
-func writeFloatToFile(file string, target float64) error {
+func WriteFloatToFile(file string, target float64) error {
 	targetString := fmt.Sprint(target)
 	if err := os.WriteFile(file, []byte(targetString), 0644); err != nil {
 		return err
