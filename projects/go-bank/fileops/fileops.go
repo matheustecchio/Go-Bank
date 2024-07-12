faac5e83c081e4496be5682dae2394d6bd8fa1a9
Matheus Tecchio <52295230+matheustecchio@users.noreply.github.com>
2024-07-12 16:57:26 +0100

    Packages (#3)
    * refactor: rename variables to be more readable
    
    * create new package `fileops` to handle file operations

--- projects/go-bank/fileops/fileops.go
+++ projects/go-bank/fileops/fileops.go
@@ -1,4 +1,4 @@
-package main
+package fileops
 
 import (
 	"fmt"
@@ -6,12 +6,12 @@ import (
 	"strconv"
 )
 
-// createFile checks if a file exists. If it doesn't, it creates the file with the designated name and format,
+// CreateFile checks if a file exists. If it doesn't, it creates the file with the designated name and format,
 // and inserts the `initialValue` into it.
 //
 // `file`: The name of the file to be created.
 // `initialValue`: The initial value to be written into the file.
-func createFile(file string, initialValue string) error {
+func CreateFile(file string, initialValue string) error {
 	if _, err := os.ReadFile(file); err != nil {
 		if _, err := os.Create(file); err != nil {
 			return err
@@ -24,33 +24,33 @@ func createFile(file string, initialValue string) error {
 	return nil
 }
 
-// getFileData reads a file and returns its content as a float64 value.
+// GetFloatFromFile reads a file and returns its content as a float64 value.
 //
 // `file`: The name of the file to be read.
 //
 // Returns the content of the file as a float64 value and an error if any operation fails.
-func getFileData(file string) (float64, error) {
-	fileByte, err := os.ReadFile(file)
+func GetFloatFromFile(file string) (float64, error) {
+	dataByte, err := os.ReadFile(file)
 	if err != nil {
 		return 0, err
 	}
 
-	byteToString := string(fileByte)
-	dataToBeSaved, err := strconv.ParseFloat(byteToString, 64)
+	dataString := string(dataByte)
+	dataFloat, err := strconv.ParseFloat(dataString, 64)
 	if err != nil {
 		return 0, err
 	}
 
-	return dataToBeSaved, nil
+	return dataFloat, nil
 }
 
-// saveDataToFile writes a float64 value to a file.
+// WriteFloatToFile writes a float64 value to a file.
 //
 // `file`: The name of the file to be written to.
 // `targetFloat`: The float64 value to be written into the file.
-func saveDataToFile(file string, targetFloat float64) error {
-	floatToString := fmt.Sprint(targetFloat)
-	if err := os.WriteFile(file, []byte(floatToString), 0644); err != nil {
+func WriteFloatToFile(file string, target float64) error {
+	targetString := fmt.Sprint(target)
+	if err := os.WriteFile(file, []byte(targetString), 0644); err != nil {
 		return err
 	}
 
--- projects/go-bank/main.go
+++ projects/go-bank/main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"log"
+
+	"github.com/matheustecchio/learn-go/fileops"
 )
 
 // balance is a global variable that holds the current balance.
 var balance float64
 
 func main() {
-	if err := createFile("balance.txt", "0"); err != nil {
+	if err := fileops.CreateFile("balance.txt", "0"); err != nil {
 		log.Fatal(err)
 	}
 
-	balance, err := getFileData("balance.txt")
+	balance, err := fileops.GetFloatFromFile("balance.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,7 +24,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		if err := saveDataToFile("balance.txt", balance); err != nil {
+		if err := fileops.WriteFloatToFile("balance.txt", balance); err != nil {
 			log.Fatal(err)
 		}
 	}
